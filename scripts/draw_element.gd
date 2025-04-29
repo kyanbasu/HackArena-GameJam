39026def1c70ec6ae9c6933c9b82fdf4ff53a80e
bnm <bartekbata1302@gmail.com>
2025-04-29 12:46:16 +0200

    placing with rotation fix + coloring placement indicator

--- scripts/draw_element.gd
+++ scripts/draw_element.gd
@@ -5,17 +5,17 @@ extends Node2D
 func _draw() -> void:
     if builder.holdingElement and builder.isHoldingElement:
         for t in builder.holdingElement.tiles:
+            var col = Color(0,1,0,0.5)
+            if builder.overlapping.has(t):
+                col = Color(1,0,0,0.5)
+                
             draw_rect(
                 Rect2(
                     floor(get_global_mouse_position().x/builder.holdingElement.TILE_SIZE)*builder.holdingElement.TILE_SIZE + builder.holdingElement.TILE_SIZE*t.x, 
                     floor(get_global_mouse_position().y/builder.holdingElement.TILE_SIZE)*builder.holdingElement.TILE_SIZE + builder.holdingElement.TILE_SIZE*t.y, 
                     builder.holdingElement.TILE_SIZE, builder.holdingElement.TILE_SIZE), 
-                Color.WHITE
+                col
             )
 
 func _process(_delta: float) -> void:
-    if builder.overlapping:
-        material.set_shader_parameter("Color", Color(1, 0, 0, 0.4))
-    else:
-        material.set_shader_parameter("Color", Color(0, 1, 0, 0.4))
     queue_redraw()
--- scripts/modules/builder.gd
+++ scripts/modules/builder.gd
@@ -11,9 +11,11 @@ const TILE_SIZE = 32
 
 # dictionary of ShipModule in matrix [x][y] representing if tile is occupied or not
 @export var occupiedSpace := {}
-var lastPartPosition : Vector2
+var lastPartPositionRotation : Vector3
 
-@export var overlapping := false
+@export var buildableSpace := {}
+
+@export var overlapping : Array[Vector2i]
 
 func _draw() -> void:
     if debugOccupied:
@@ -28,6 +30,11 @@ var holdingElement : ShipModule
 var isHoldingElement := false
 
 func _ready() -> void:
+    for x in range(-4,5):
+        for y in range(-4,5):
+            if !buildableSpace.has(x):
+                buildableSpace[x] = {}
+            buildableSpace[x][y] = true
     var p = parts[1].instantiate()
     add_child(p)
     place_part(p, Vector2(500, 200))
@@ -64,9 +71,11 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
     if !active: return
+    overlapping = []
     if holdingElement and isHoldingElement:
         holdingElement.global_position = lerp(holdingElement.global_position, get_global_mouse_position(), delta*10*clamp(distance(holdingElement.global_position, get_global_mouse_position())/1000, 1, 10))
-        overlapping = any_overlap(holdingElement, get_global_mouse_position())
+        overlapping = get_overlap(holdingElement, get_global_mouse_position())
+        
     
     queue_redraw()
 
@@ -74,13 +83,22 @@ func any_overlap(part: ShipModule, _position: Vector2) -> bool:
     for t in part.tiles:
         var x = t.x + int(floor(_position.x/part.TILE_SIZE))
         var y = t.y + int(floor(_position.y/part.TILE_SIZE))
-        if occupiedSpace.has(x) and occupiedSpace[x].has(y):
+        if (occupiedSpace.has(x) and occupiedSpace[x].has(y)) or (!buildableSpace.has(x) or !buildableSpace[x].has(y)):
             return true
     return false
 
+func get_overlap(part: ShipModule, _position: Vector2) -> Array[Vector2i]:
+    var _o : Array[Vector2i] = []
+    for t in part.tiles:
+        var x = t.x + int(floor(_position.x/part.TILE_SIZE))
+        var y = t.y + int(floor(_position.y/part.TILE_SIZE))
+        if (occupiedSpace.has(x) and occupiedSpace[x].has(y)) or (!buildableSpace.has(x) or !buildableSpace[x].has(y)):
+            _o.append(t)
+    return _o
+
 # position will be rounded to grid
 func pickup_part(part: ShipModule):
-    lastPartPosition = part.global_position
+    lastPartPositionRotation = Vector3(part.global_position.x, part.global_position.y, part.rotation)
     for t in part.tiles:
         var x = t.x + int(floor(part.global_position.x/part.TILE_SIZE))
         var y = t.y + int(floor(part.global_position.y/part.TILE_SIZE))
@@ -90,18 +108,21 @@ func pickup_part(part: ShipModule):
                 occupiedSpace.erase(x)
 
 # position will be rounded to grid
-func place_part(part: ShipModule, _position: Vector2):
+func place_part(part: ShipModule, _position: Vector2, _rotation: float=-1):
+    if _rotation != -1:
+        while (int(abs(part.rotation - _rotation)*180/PI))%360 > 10:
+            part.rotate_left()
     part.global_position = Vector2(
         floor(_position.x/part.TILE_SIZE)*part.TILE_SIZE + part.TILE_SIZE - part.TILE_SIZE/2,
         floor(_position.y/part.TILE_SIZE)*part.TILE_SIZE + part.TILE_SIZE - part.TILE_SIZE/2
     )
     #check for collisions
-    for t in part.tiles:
-        var x = t.x + int(floor(part.global_position.x/part.TILE_SIZE))
-        var y = t.y + int(floor(part.global_position.y/part.TILE_SIZE))
-        if occupiedSpace.has(x) and occupiedSpace[x].has(y): #collision appeared
-            place_part(part, lastPartPosition)
+    if any_overlap(part, _position):
+        var new_pos = Vector2(lastPartPositionRotation.x, lastPartPositionRotation.y)
+        if new_pos == _position:
             return
+        place_part(part, new_pos, lastPartPositionRotation.z)
+        return
     #reserve space
     for t in part.tiles:
         var x = t.x + int(floor(part.global_position.x/part.TILE_SIZE))
