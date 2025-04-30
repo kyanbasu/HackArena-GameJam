dc1b991c83feae06c6418e9e12012a975ae33332
bnm <bartekbata1302@gmail.com>
2025-04-30 17:21:29 +0200

    rewrite to space using vectors
    + needs testing
    
    nested dictionaries were trash

--- scripts/modules/builder.gd
+++ scripts/modules/builder.gd
@@ -9,21 +9,20 @@ class_name Builder
 
 const TILE_SIZE = 32
 
-# dictionary of ShipModule in matrix [x][y] representing if tile is occupied or not
-@export var occupiedSpace := {}
+# dictionary of ShipModule in Vector2i(x,y) representing if tile is occupied or not
+@export var occupiedSpace : Dictionary[Vector2i, ShipModule] = {}
 var lastPartPositionRotation : Vector3
 
-@export var buildableSpace := {}
+@export var buildableSpace : Dictionary[Vector2i, bool] = {}
 
 @export var overlapping : Array[Vector2i]
 
 func _draw() -> void:
     if debugOccupied:
-        for x in occupiedSpace.keys():
-            for y in occupiedSpace[x].keys():
-                draw_rect(
-                Rect2(TILE_SIZE*x-2, TILE_SIZE*y-2, TILE_SIZE+4, TILE_SIZE+4), 
-                Color(1, 0, 0, 0.5), false, 6)
+        for v in occupiedSpace.keys():
+            draw_rect(
+            Rect2(TILE_SIZE*v.x-2, TILE_SIZE*v.y-2, TILE_SIZE+4, TILE_SIZE+4), 
+            Color(1, 0, 0, 0.5), false, 6)
 #@export var modules : Dictionary[int, ShipModule]
 
 var selectedModule : ShipModule
@@ -34,9 +33,7 @@ func _ready() -> void:
     
     for x in range(-5,6):
         for y in range(-4,5):
-            if !buildableSpace.has(x):
-                buildableSpace[x] = {}
-            buildableSpace[x][y] = true
+            buildableSpace[Vector2i(x,y)] = true
 
 func _input(event: InputEvent) -> void:
     if !active: return
@@ -63,24 +60,24 @@ func _process(delta: float) -> void:
     if selectedModule and isHoldingModule:
         selectedModule.global_position = lerp(selectedModule.global_position, get_global_mouse_position(), delta*10*clamp(distance(selectedModule.global_position, get_global_mouse_position())/1000, 1, 10))
         overlapping = get_overlap(selectedModule, get_global_mouse_position())
-        
     
     queue_redraw()
 
-func any_overlap(part: ShipModule, _position: Vector2) -> bool:
+func any_overlap(part: ShipModule, pos: Vector2) -> bool:
     for t in part.tiles:
-        var x = t.x + int(floor(_position.x/part.TILE_SIZE))
-        var y = t.y + int(floor(_position.y/part.TILE_SIZE))
-        if (occupiedSpace.has(x) and occupiedSpace[x].has(y)) or (!buildableSpace.has(x) or !buildableSpace[x].has(y)):
+        var chk = t + world_to_grid(pos)
+        if occupiedSpace.has(chk) or !buildableSpace.has(chk):
             return true
     return false
 
-func get_overlap(part: ShipModule, _position: Vector2) -> Array[Vector2i]:
+func world_to_grid(pos: Vector2) -> Vector2i:
+    return Vector2i(floor(pos.x/TILE_SIZE), floor(pos.y/TILE_SIZE))
+
+func get_overlap(part: ShipModule, pos: Vector2) -> Array[Vector2i]:
     var _o : Array[Vector2i] = []
     for t in part.tiles:
-        var x = t.x + int(floor(_position.x/part.TILE_SIZE))
-        var y = t.y + int(floor(_position.y/part.TILE_SIZE))
-        if (occupiedSpace.has(x) and occupiedSpace[x].has(y)) or (!buildableSpace.has(x) or !buildableSpace[x].has(y)):
+        var chk = t + world_to_grid(pos)
+        if occupiedSpace.has(chk) or !buildableSpace.has(chk):
             _o.append(t)
     return _o
 
@@ -98,8 +95,8 @@ func is_part_adjacent(part: ShipModule) -> bool:
     return false
 
 func is_part_adjacent_check_one(part: ShipModule, offset: Vector2i) -> bool:
-    var checkPos = Vector2i(part.global_position/part.TILE_SIZE - Vector2(.5,.5)) + offset
-    if occupiedSpace.has(checkPos.x) and occupiedSpace[checkPos.x].has(checkPos.y) and occupiedSpace[checkPos.x][checkPos.y] != part:
+    var chk = Vector2i(part.global_position/part.TILE_SIZE - Vector2(.5,.5)) + offset
+    if occupiedSpace.has(chk) and occupiedSpace[chk] != part:
         return true
     return false
 
@@ -108,12 +105,9 @@ func pickup_part(part: ShipModule):
     selectedModule.z_index = 10
     lastPartPositionRotation = Vector3(part.global_position.x, part.global_position.y, part.rotation)
     for t in part.tiles:
-        var x = t.x + int(floor(part.global_position.x/part.TILE_SIZE))
-        var y = t.y + int(floor(part.global_position.y/part.TILE_SIZE))
-        if occupiedSpace.has(x) and occupiedSpace[x].has(y):
-            occupiedSpace[x].erase(y)
-            if occupiedSpace[x].is_empty():
-                occupiedSpace.erase(x)
+        var chk = t + Vector2i(part.global_position/part.TILE_SIZE - Vector2(.5,.5))
+        if occupiedSpace.has(chk):
+            occupiedSpace.erase(chk)
     isHoldingModule = true
 
 # position will be rounded to grid
@@ -143,12 +137,8 @@ func place_part(part: ShipModule, _position: Vector2, _rotation: float=-1):
         return
     #reserve space
     for t in part.tiles:
-        var x = t.x + int(floor(part.global_position.x/part.TILE_SIZE))
-        var y = t.y + int(floor(part.global_position.y/part.TILE_SIZE))
-        if !occupiedSpace.has(x):
-            occupiedSpace[x] = {y: part}
-        else:
-            occupiedSpace[x][y] = part
+        var v = t + Vector2i(part.global_position/part.TILE_SIZE - Vector2(.5,.5))
+        occupiedSpace[v] = part
 
 func distance(v1: Vector2, v2: Vector2) -> float:
     return sqrt((v1.x + v2.x)**2 + (v1.y + v2.y)**2);
--- scripts/space_grid_draw.gd
+++ scripts/space_grid_draw.gd
@@ -3,16 +3,15 @@ extends Node2D
 @export var builder : Builder
 
 func _draw() -> void:
-    for x in builder.buildableSpace.keys():
-        for y in builder.buildableSpace[x].keys():
-            var col = Color(1,1,0,0.2)
-            draw_rect(
-                Rect2(
-                    builder.TILE_SIZE*x, 
-                    builder.TILE_SIZE*y, 
-                    builder.TILE_SIZE, builder.TILE_SIZE), 
-                col
-            )
+    for v in builder.buildableSpace.keys():
+        var col = Color(1,1,0,0.2)
+        draw_rect(
+            Rect2(
+                builder.TILE_SIZE*v.x, 
+                builder.TILE_SIZE*v.y,
+                builder.TILE_SIZE, builder.TILE_SIZE), 
+            col
+        )
 
 func _process(_delta: float) -> void:
     queue_redraw()
